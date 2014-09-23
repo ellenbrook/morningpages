ef1c95d30b31258f1749c172dfcfebcfad83e1b5
Eric Ellenbrook <ericellenbrook@gmail.com>
2014-09-23 03:08:43 -0400

    Messing with the DB the PHP way

--- application/classes/Controller/Cron/Maintenance.php
+++ application/classes/Controller/Cron/Maintenance.php
@@ -1,2 +1,10 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-class Controller_Cron_Maintenance extends Controller {		public function action_index()	{		maintenance::reminders();	}	}
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Cron_Maintenance extends Controller {
+	
+	public function action_index()
+	{
+		maintenance::reminders();
+	}
+	
+}
