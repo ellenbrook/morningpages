3dd2a586b076e22200b035f2c0ab5b352ea5437d
Eric Ellenbrook <ericellenbrook@gmail.com>
2014-09-24 17:52:34 -0400

    Stupid log file fuckup

---
+++ application/logs/2014/09/23.php
@@ -0,0 +1,134 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2014-09-23 00:46:20 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 00:46:20 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 00:55:04 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 00:55:04 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 00:56:08 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 00:56:08 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 00:56:35 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 00:56:35 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 00:56:42 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 00:56:42 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 01:52:28 --- EMERGENCY: ErrorException [ 4 ]: syntax error, unexpected 'DB' (T_STRING) ~ APPPATH/classes/Controller/Cron/Maintenance.php [ 13 ] in file:line
+2014-09-23 01:52:28 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
+2014-09-23 01:53:00 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 01:53:00 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 01:56:11 --- EMERGENCY: ErrorException [ 1 ]: Class 'DATABASE' not found ~ APPPATH/classes/Controller/Cron/Maintenance.php [ 17 ] in file:line
+2014-09-23 01:56:11 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
+2014-09-23 01:56:41 --- EMERGENCY: ErrorException [ 1 ]: Undefined class constant 'SELECT' ~ APPPATH/classes/Controller/Cron/Maintenance.php [ 17 ] in file:line
+2014-09-23 01:56:41 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
+2014-09-23 01:56:55 --- EMERGENCY: ErrorException [ 1 ]: Call to a member function as_array() on integer ~ APPPATH/classes/Controller/Cron/Maintenance.php [ 17 ] in file:line
+2014-09-23 01:56:55 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main} in file:line
+2014-09-23 02:08:08 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 02:08:08 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 02:38:53 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 02:38:53 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 02:39:01 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 02:39:01 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 02:39:05 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 02:39:05 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 02:39:08 --- EMERGENCY: Kohana_Exception [ 0 ]: The wordcount property does not exist in the Model_Page class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 02:39:08 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('wordcount')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(17): Kohana_ORM->__get('wordcount')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 08:28:39 --- EMERGENCY: Kohana_Exception [ 0 ]: The theme property does not exist in the Model_User class ~ MODPATH/orm/classes/Kohana/ORM.php [ 687 ] in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
+2014-09-23 08:28:39 --- DEBUG: #0 /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php(603): Kohana_ORM->get('theme')
+#1 /home/forge/dev.morningpages.net/application/classes/Controller/Ajax/User.php(21): Kohana_ORM->__get('theme')
+#2 /home/forge/dev.morningpages.net/system/classes/Kohana/Controller.php(84): Controller_Ajax_User->action_info()
+#3 [internal function]: Kohana_Controller->execute()
+#4 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client/Internal.php(97): ReflectionMethod->invoke(Object(Controller_Ajax_User))
+#5 /home/forge/dev.morningpages.net/system/classes/Kohana/Request/Client.php(114): Kohana_Request_Client_Internal->execute_request(Object(Request), Object(Response))
+#6 /home/forge/dev.morningpages.net/system/classes/Kohana/Request.php(986): Kohana_Request_Client->execute(Object(Request))
+#7 /home/forge/dev.morningpages.net/index.php(119): Kohana_Request->execute()
+#8 {main} in /home/forge/dev.morningpages.net/modules/orm/classes/Kohana/ORM.php:603
