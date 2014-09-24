bf6f696e4c1aec08416ae38b34fac0f1505aef52
Eric <ericellenbrook@gmail.com>
2014-09-24 19:10:32 -0400

    Add user option tabs on options page

--- application/views/Page/options.php
+++ application/views/Page/options.php
@@ -1,1 +1,13 @@
-Hello
+<section class="options-page">
+	<nav class="user-options-nav">
+		<ul>
+			<li class="tab active-tab"><a href="personal-settings">Personal Settings</a></li>
+			<li class="tab"><a href="site-settings">Site Settings</a></li>
+			<li class="tab"><a href="account-settings">Account Settings</a></li>
+		</ul>
+	</nav>
+	<!--modal footer -->
+	<div class="pull-right">
+		<button name="account-deletion" class="btn-good pull-right">Save info</button>
+	</div>
+</section> <!-- options modal -->
