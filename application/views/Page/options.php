5bfed3092fb6ee4deda8726972d506ae436f7ab9
Eric <ericellenbrook@gmail.com>
2014-09-24 20:26:35 -0400

    tabs JS

--- application/views/Page/options.php
+++ application/views/Page/options.php
@@ -1,14 +1,30 @@
 <section class="options-page">
 	<nav class="user-options-nav">
-		<ul>
-			<li class="tab active-tab"><a href="personal-settings">Personal Settings</a></li>
-			<li class="tab"><a href="site-settings">Site Settings</a></li>
-			<li class="tab"><a href="account-settings">Account Settings</a></li>
+		<ul data-bind="tabs:'tab-container'">
+		   <li class="tab active-tab">
+		       <a href="#personal-settings">Personal Settings</a>
+		   </li>
+		   <li class="tab">
+		       <a href="#site-settings">Site Settings</a>
+		   </li>
+		   <li class="tab">
+		       <a href="#account-settings">Account Settings</a>
+		   </li>
 		</ul>
-	</nav>
-	
-	<form>
 
+	</nav>
+
+	<form>
+		<div class="tab-container" id="personal-settings">
+			Personal settings
+		</div>
+		<div class="tab-container" id="site-settings">
+			Site settings
+		</div>
+		<div class="tab-container" id="account-settings">
+			Account settings
+		</div>
+	
 		<div class="pull-right">
 			<button name="account-deletion" class="btn-good pull-right">Save info</button>
 		</div>
--- media/js/bindings.js
+++ media/js/bindings.js
@@ -20,7 +20,23 @@ define([
 	        });
 	    }
 	};
-	 
+	
+	ko.bindingHandlers.tabs = {
+        init:function(element, targets){
+            console.log('Works so far');
+            $(element).find('a').each(function(){
+                console.log('found an a');
+                $(this).on('click', function(){
+                    console.log('clicked an a');
+                    var $targets = $('.'+targets()).hide();
+                    $(element).find('active-tab').removeClass('active-tab');
+                    $(this).parent().addClass('active-tab');
+                    $($(this).attr('href')).show();
+                });
+            });
+        }
+    };
+
 	ko.bindingHandlers.showModal = {
 		init:function(element, valueAccessor){
 			$(element).on('click',function(){
