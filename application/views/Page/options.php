20dea538dba9dd1818ff10ff65be3552d07c2824
Eric <ericellenbrook@gmail.com>
2014-09-25 13:00:37 -0400

    Add form details

--- application/views/Page/options.php
+++ application/views/Page/options.php
@@ -16,15 +16,78 @@
 
 	<form>
 		<div class="tab-container" id="personal-settings">
-			Personal settings
+			<fieldset>
+				<legend>User Options</legend>
+
+				<label for="email">Email</label>
+					<input type="text" id="email" name="email">
+
+				<label for="new-password">New Password</label>
+					<input type="password" id="new-password" name="new-password">
+
+				<label for="confirm-new-password">New Password Confirm</label>
+					<input type="password" id="confirm-new-password" name="confirm-new-password">
+			</fieldset>
 		</div>
 		<div class="tab-container" id="site-settings">
-			Site settings
+			<fieldset>
+				<legend>Site Options</legend>
+
+				<label for="site-theme">Theme</label>
+					<select id="site-theme" name="site-theme">
+						<option value="1">Theme 1</option>
+						<option value="2">Theme 2</option>
+						<option value="3">Theme 3</option>
+					</select>
+
+				<label for="reminder-email">Daily email reminder</label>
+					<input id="reminder-email" type="checkbox" name="reminder-email">
+					 at 
+					<select>
+						<option>1</option>
+						<option>2</option>
+						<option>3</option>
+						<option>4</option>
+						<option>5</option>
+						<option>6</option>
+						<option>7</option>
+						<option>8</option>
+						<option>9</option>
+						<option>10</option>
+						<option>11</option>
+						<option>12</option>
+					</select>:<select>
+						<option>00</option>
+						<option>15</option>
+						<option>30</option>
+						<option>45</option>
+					</select>
+					<select>
+						<option>AM</option>
+						<option>PM</option>
+					</select>
+
+				<label for="privacy-mode">Privacy Mode</label>
+					<input id="privacy-mode" name="privacy-mode" type="checkbox">
+
+				<label for="hemingway-mode">Hemingway Mode</label>
+					<input id="hemingway-mode" id="hemingway-mode" type="checkbox">
+
+			</fieldset>
 		</div>
 		<div class="tab-container" id="account-settings">
-			Account settings
+			<fieldset>
+				<legend>User Options</legend>
+
+				<label for="file">Filename:</label>
+					<input type="file" name="file" id="file">
+				<br>
+				<button class="btn btn-good">Export Posts (XML format)</button> 
+				<br>
+				<a href="#">Delete Account</a>
+			</fieldset>
 		</div>
-	
+
 		<div class="pull-right">
 			<button name="account-deletion" class="btn-good pull-right">Save info</button>
 		</div>
