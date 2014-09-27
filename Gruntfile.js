eeb14d6416e5504f50b7c12bf6608cc9a75224e0
Sakto <daniel@danwebs.dk>
2014-09-27 20:25:55 +0200

    gruntfile will run watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt){
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['sass']);
-	//grunt.registerTask('default', ['watch']);
+	//grunt.registerTask('default', ['sass']);
+	grunt.registerTask('default', ['watch']);
 };
 
