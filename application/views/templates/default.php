0d523f18e6afbfdad58af5fb579a2df430fbf21d
Eric <ericellenbrook@gmail.com>
2014-09-30 16:22:49 -0400

    markdown stylings and modal fixes

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -201,13 +201,14 @@
 								<label class="modal-close" for="tips-tricks"></label>
 								<h3>Tips and tricks</h3>
 								<dl>
-									<dt>Ctrl/Cmd + Spacebar</dt>
-									<dd>We take privacy very seriously. Use this shortcut if you're in the middle of writing and somebody walks into the room, begins to peer over your shoulder, or tries to see what you're writing. This will bring up the text from a random Wikipedia article.</dd>
+									<dt>Ctrl + Spacebar (both Mac and PC)</dt>
+									<dd>We take privacy very seriously. Use this shortcut if you're in the middle of writing and somebody walks into the room, begins to peer over your shoulder, or tries to see what you're writing. This replaces the text you've written with the text from a random Wikipedia article.</dd>
 					
 									<dt>Markdown</dt>
 									<dd>What is markdown? Markdown is simply a text-formatting syntax used to format text on the web without having to worry about HTML. Think of it as an easy and natural way to to format your text without the need of using learning code.</dd>
-									<dd>Although the idea behind Morning Pages is stream of consciousness thoughts, sometimes those thoughts would be better off with a bit of organization and that's where Markdown comes in handy.</dd>
-									<dd>For an in-depth listing of all of Markdown's features, check out <a href="http://daringfireball.net/projects/markdown/syntax" target="_blank" rel="nofollow">Markdown Syntax Basics</a>.</dd>
+
+									<dt>Markdown Basics</dt>
+									<dd>For an in-depth listing of all of Markdown's features, check out <a href="#" rel="nofollow">our forum post</a>.</dd>
 									</dd>
 								</dl>
 								</div>
