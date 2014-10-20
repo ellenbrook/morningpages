55e7394de2dbc6f0b8698233a8cae845a611744f
Sakto <daniel@danwebs.dk>
2014-10-20 12:28:06 +0200

    Removing circular dependencies site/user

--- media/js/viewModels/User/options.js
+++ media/js/viewModels/User/options.js
@@ -1,17 +1,40 @@
 define([
 	'jquery',
 	'knockout',
-	'plugins/refill',
-	'models/user'
-],function($, ko, refill, user){
+	'site',
+	'plugins/refill'
+],function($, ko, site, refill){
 	
 	var optionsModel = function(){
 		var self = this;
 		
-		self.user = new user();
+		self.user = site.user;
 		self.user.getInfo();
 		
+		self.save_setting = function(setting){
+			self.user.save_setting(setting).done(function(reply){
+				reply = $.parseJSON(reply);
+				site.say(reply);
+			});
+		};
+		
+		self.save_theme = function(){
+			$.post('/ajax/user/savetheme',{id:self.user.options.theme_id()},function(reply){
+				if(!reply.success)
+				{
+					site.say(reply);
+				}
+				else
+				{
+					site.theme(reply.theme);
+				}
+			},'json');
+		};
+		
 		$('#site-options').tabs();
+		
+		
+		
 		
 	};
 	
