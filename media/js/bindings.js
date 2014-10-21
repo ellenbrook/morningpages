0167029071a00bc5d45865b59ac667902c8db9c8
Sakto <daniel@danwebs.dk>
2014-10-21 14:22:53 +0200

    Hamburger custom binding

--- media/js/bindings.js
+++ media/js/bindings.js
@@ -21,6 +21,16 @@ define([
 	    }
 	};
 	
+	ko.bindingHandlers.ImAburger = {
+		init:function(element) {
+			$(element).on('click',function(){
+				$(this).find('.hamburger-line:nth-child(2)').toggleClass('rotate-left');
+				$(this).find('.hamburger-line:nth-child(3)').toggleClass('rotate-right');
+				$(this).find('.hamburger-line:nth-child(1)').toggleClass('hide-me');
+			});
+		}
+	};
+	
 	ko.bindingHandlers.fadeVisible = {
 		init: function (element, valueAccessor) {
 			var value = valueAccessor();
