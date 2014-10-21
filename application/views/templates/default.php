e22e5d585169bf5609e2590878f74eb5de62782d
Eric <ericellenbrook@gmail.com>
2014-10-21 13:16:56 -0400

    Mobile menu cross-site

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -31,6 +31,76 @@
 		  <div class="hamburger-line"></div>
 		  <div class="hamburger-line"></div>
 		</div>
+		<div class="mobile-navigation">
+			<ul>
+				<li><a href="#about">About</a></li>
+				<li><a href="<?php echo URL::site('write'); ?>" title="Write" class="<?php echo ($controller=='Page'?' active':''); ?>">Write</a></li>
+				<li><a href="<?php echo URL::site('talk'); ?>" title="Discuss Morning Pages">Talk</a></li>
+				<li class="hidden" data-bind="visible:!site.user.logged()"><a href="#" data-bind="click:showLoginModal">Login</a></li>
+				<li class="hidden" data-bind="visible:!site.user.logged()"><a href="#" data-bind="click:showRegisterModal">Register</a></li>
+				<li class="hidden" data-bind="visible:site.user.logged()"><a href="<?php echo URL::site('me'); ?>" title="Your personal Morning Pages profile">Me</a></li>
+				<li class="header">Options</li>
+				<?php if(user::logged()): ?>
+				<li><a href="<?php echo URL::site('user/options'); ?>">User options</a></li>
+				<!-- <li>Current streak: echo user::get()->current_streak </li> -->
+				<li>
+			    	<select data-bind="event:{change:goToPreviousPage}" id="pastposts" class="mobile-select">
+			        	<option value="0">Previous pages</option>
+			        	<option value="/">Today</option>
+<?php
+    					$pages = user::get()
+    						->pages
+    						->where('type','=','page')
+    						->order_by('created', 'DESC')
+    						->find_all();
+    					$years = array();
+    					if((bool)$pages->count())
+    					{
+    						foreach($pages as $p)
+    						{
+    							$stamp = $p->created;
+    							$year = date('Y', $stamp);
+    							if(!array_key_exists($year, $years))
+    							{
+    								$years[$year] = array();
+    							}
+    							$month = date('F',$stamp);
+    							if(!array_key_exists($month, $years[$year]))
+    							{
+    								$years[$year][$month] = array();
+    							}
+    							$years[$year][$month][] = $p;
+    						}
+    					}
+    					foreach($years as $year => $month)
+    					{
+    						foreach($month as $monthname => $days)
+    						{
+    							echo '<optgroup label="'.$monthname.', '.$year.'">';
+    							foreach($days as $day)
+    							{
+    							    $dayname = date('l ',$day->created).' the '.date('jS',$day->created);
+                                    if($day->day != site::today_slug())
+                                    {
+                                        //$dayname = 'Today';
+                                        echo '<option value="'.$day->day.'"'.($dayname==$day->day?' selected="selected"':'').'>'.$dayname.'</option>';
+                                    }
+    								
+    							}
+    							echo '<optgroup>';
+    						}
+    					}
+?>
+               		</select>
+				</li>
+			
+				<li><a href="<?php echo URL::site('user/logout'); ?>" title="Log out">Log out</a></li>
+			<?php else: ?>
+				<li><a href="#" data-bind="click:showTipsAndTricks">Tips and Tricks</a></li>
+				<li>Log in</li>
+			<?php endif; ?>
+			</ul>
+		</div>
 		<nav class="default">
 			<ul>
 				<li><a href="#about">About</a></li>
