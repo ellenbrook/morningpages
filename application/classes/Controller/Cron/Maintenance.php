5939bc96d7054c8337a00ce7bec90a97aa93eee9
Sakto <daniel@danwebs.dk>
2014-11-08 15:59:59 +0100

    Adding slugs

--- application/classes/Controller/Cron/Maintenance.php
+++ application/classes/Controller/Cron/Maintenance.php
@@ -4,7 +4,21 @@ class Controller_Cron_Maintenance extends Controller {
 	
 	public function action_index()
 	{
-		maintenance::reminders();
+		$users = ORM::factory('User')->find_all();
+		foreach($users as $user)
+		{
+			$slug = site::slugify($user->username);
+			$orgslug = $slug;
+			$existing = ORM::factory('User')->where('slug','=',$slug)->find();
+			$i = 2;
+			while($existing->loaded())
+			{
+				$slug = $orgslug.'-'.$i;
+				$i++;
+			}
+			$user->slug = $slug;
+			$user->update();
+		}
 	}
 	
 }
