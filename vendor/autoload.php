bac046255b09eecb44612fd2e9c7c412ca8cd759
Eric Ellenbrook <ericellenbrook@gmail.com>
2014-11-08 12:22:48 -0500

    Catch /talk validation errors

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit0be30f2b3692f1533ea708e3bfbef1c2::getLoader();
+return ComposerAutoloaderInit58ae9920b2c4ea78a0d90df5a99f3c39::getLoader();
