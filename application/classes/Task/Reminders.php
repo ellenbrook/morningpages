e7041e58dceb282864ec70ebe4f3fd4a12888b87
Sakto <daniel@danwebs.dk>
2014-12-22 10:30:24 +0100

    Making sure only 1 reminder is sent

--- application/classes/Task/Reminders.php
+++ application/classes/Task/Reminders.php
@@ -29,27 +29,32 @@ class Task_Reminders extends Minion_Task {
 		$results = $query->execute()->as_array();
 		if(is_array($results))
 		{
+			$sent = array();
 			foreach($results as $result)
 			{
 				$user = ORM::factory('User', arr::get($result,'id',''));
 				if($user->loaded())
 				{
-					// Send the mail here..
-					$mail = mail::create('reminder')
-						->to($user->email)
-						->tokenize(array(
-							'username' => $user->username,
-							'writeurl' => 'http://morningpages.net/write',
-							'link' => HTML::anchor('http://morningpages.net/write','It’s time to write your Morning Pages'),
-							'contactlink' => HTML::anchor('http://morningpages.net/contact','contact us') )
-						)
-						->send();
-					
-					// Then:
-					$option = $user->option;
-					$option->last_reminder = time();
-					$option->next_reminder = $user->get_next_reminder(true);
-					$option->save();
+					if(!in_array($user->email, $sent))
+					{
+						$sent[] = $user->email;
+						// Send the mail here..
+						$mail = mail::create('reminder')
+							->to($user->email)
+							->tokenize(array(
+								'username' => $user->username,
+								'writeurl' => 'http://morningpages.net/write',
+								'link' => HTML::anchor('http://morningpages.net/write','It’s time to write your Morning Pages'),
+								'contactlink' => HTML::anchor('http://morningpages.net/contact','contact us') )
+							)
+							->send();
+						
+						// Then:
+						$option = $user->option;
+						$option->last_reminder = time();
+						$option->next_reminder = $user->get_next_reminder(true);
+						$option->save();
+					}
 				}
 			}
 		}
