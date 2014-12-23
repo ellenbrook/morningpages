65fc4d9fdf8cae0c064501323dca11b45f880a03
Sakto <daniel@danwebs.dk>
2014-12-23 12:49:47 +0100

    404 page markup

--- application/views/Errors/404.php
+++ application/views/Errors/404.php
@@ -1,15 +1,4 @@
-<div class="row-fluid">
-	<div class="span12">
-		<ul class="breadcrumb">
-			<li>
-				<?php echo HTML::anchor('', site::name());?>
-			</li>
-			<li class="active">Write</li>
-		</ul>
-	</div>
-</div>
-
-<h1>404 - Page not found</h1>
+<h2>404 - Page not found</h2>
 
 <p>
 	The page you're looking for could not be found! You may have followed an outdated link or mistyped the address. Please check the URL and try again.
