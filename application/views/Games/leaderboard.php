5abcc1596139b4948d7bb0650fdd65311013edf7
Sakto <daniel@danwebs.dk>
2015-01-09 10:56:09 +0100

    Better? layout for the leaderboard

--- application/views/Games/leaderboard.php
+++ application/views/Games/leaderboard.php
@@ -1,4 +1,32 @@
 <div class="half">
+	
+	<h2>Most points</h2>
+	<table class="table-borders">
+		<thead>
+			<th>Username</th>
+			<th>Points</th>
+		</thead>
+		<tbody>
+<?php
+			foreach($points as $point)
+		    {
+		        $user = ORM::factory('user', arr::get($point, 'id'));
+		        if($user->loaded())
+		        {
+		           echo '<tr>';
+		           echo '<td>'.$user->link().'</td>';
+		           echo '<td>'.arr::get($point, 'points', 0).'</td>';
+		           echo '</tr>';
+		        }
+		    }
+?>
+		</tbody>
+	</table>
+	
+</div>
+
+<div class="half">
+	
 	<h2>Current Streak</h2>
 
 	<table class="table-borders">
@@ -23,9 +51,7 @@
 ?>
 		</tbody>
 	</table>
-</div>
-
-<div class="half">
+	
 	<h2>Most active on <a href="/talk">/talk</a></h2>
 	<table class="table-borders">
 		<thead>
@@ -44,33 +70,6 @@
 		           echo '<tr>';
 		           echo '<td>'.$user->link().'</td>';
 		           echo '<td>'.arr::get($a, 'posts', 0).'</td>';
-		           echo '</tr>';
-		        }
-		    }
-?>
-		</tbody>
-	</table>
-</div>
-
-<hr />
-
-<div class="half">
-	<h2>Most points</h2>
-	<table class="table-borders">
-		<thead>
-			<th>Username</th>
-			<th>Points</th>
-		</thead>
-		<tbody>
-<?php
-			foreach($points as $point)
-		    {
-		        $user = ORM::factory('user', arr::get($point, 'id'));
-		        if($user->loaded())
-		        {
-		           echo '<tr>';
-		           echo '<td>'.$user->link().'</td>';
-		           echo '<td>'.arr::get($point, 'points', 0).'</td>';
 		           echo '</tr>';
 		        }
 		    }
