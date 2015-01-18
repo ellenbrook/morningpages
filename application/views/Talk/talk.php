28983c00f505977f75bc2c0b53395bccecdbb8f4
Sakto <saktomail@gmail.com>
2015-01-18 10:51:35 +0100

    Dont show streak on /talk (for OP)

--- application/views/Talk/talk.php
+++ application/views/Talk/talk.php
@@ -39,9 +39,6 @@ echo $talk->pagination($currentpage);
 					<p>
 						<?php echo $talk->user->points(); ?> point<?php echo ($talk->user->points()==1?'':'s'); ?>
 					</p>
-					<p>
-						Current streak: <?php echo $talk->user->current_streak; ?>
-					</p>
 					<p class="comment-detail">
 						<?php echo Date::fuzzy_span($talk->created); ?>
 					</p>
