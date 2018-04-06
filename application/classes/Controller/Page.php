b5078ba5b2b6bc5c16344bccdf2670fec187a2c4
Daniel Valentin <saktomail@gmail.com>
2018-04-06 19:57:31 +0200

    Donations - Blind

--- application/classes/Controller/Page.php
+++ application/classes/Controller/Page.php
@@ -1,24 +1,30 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Page extends Controller_Project {
-	
+
 	public function action_anonymouswriting()
 	{
 		$this->template->view = View::factory('Page/write');
 	}
-	
+
 	public function action_about()
 	{
 		seo::instance()->title("About Morning Pages");
 		seo::instance()->description("Morning Pages is about writing three pages of stream of consciousness thought every day. Become a better person by using MorninPages.net");
 	}
-	
+
+	public function action_donations()
+	{
+		seo::instance()->title("Donations to Morningpages");
+		seo::instance()->description("Donations made to the continual running of Morningpages, by it's generous users");
+	}
+
 	public function action_terms()
 	{
 		seo::instance()->title("Terms of Service");
 		seo::instance()->description("Morning Pages terms of service.");
 	}
-	
+
 	public function action_privacy()
 	{
 		seo::instance()->title("Morning Pages Privacy Policy");
@@ -27,9 +33,9 @@ class Controller_Page extends Controller_Project {
 
 	public function action_test()
 	{
-		
+
 	}
-	
+
 	public function action_contact()
 	{
 		$errors = false;
@@ -42,7 +48,7 @@ class Controller_Page extends Controller_Project {
 			$val->rule('suggestion', 'not_empty');
 			if($val->check())
 			{
-				
+
 				notes::success('Your message has been sent and we will get back to you as soon as possible. Thanks!');
 				$mail = mail::create('suggestion')
 					->to('morningpagesnet@gmail.com')
@@ -62,11 +68,11 @@ class Controller_Page extends Controller_Project {
 		seo::instance()->title("Contact Morning Pages");
 		seo::instance()->description("Feel free to contact MorningPages.net if you have questions or concerns about your account, the site or for more information regarding your Morning Pages.");
 	}
-	
+
 	public function action_faq()
 	{
 		seo::instance()->title("Morning Pages Questions");
 		seo::instance()->description("Frequently asked questions regarding your Morning Pages, the Morning Pages website, and more. Don't see your listed? Check the Morning Pages forum for more info.");
 	}
-	
+
 }
